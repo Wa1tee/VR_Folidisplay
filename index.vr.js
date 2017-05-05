80da5c48e7319caecffdc636b119dab44d13b221
Wa1tee <antti.auranen@iki.fi>
2017-05-05 11:50:59 +0300

    initial commit

---
+++ index.vr.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  StyleSheet,
+  Pano,
+  Text,
+  View,
+  VrButton,
+} from 'react-vr';
+
+export default class vr_workshop extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foliData: [],
+      backColor: '#99003d',
+    }
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+  
+  fetchData(){
+    console.log("fetching...");
+    fetch("https://data.foli.fi/siri/sm/T42")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        this.setState((prevState) => ({ foliData: (res.result) }));
+      }).catch((error) => {
+        console.log("Big Network Error");
+        console.log(error.message);
+      })
+  }
+
+  render() {
+    return (
+      <View>
+        <Pano source={asset('matador.jpg')}/>
+        <Text
+          style={{
+            backgroundColor: '#2E2EFEaf',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -5]}],
+          }}>
+        Destination: {this.state.foliData.length > 0 ? this.state.foliData[0].destinationdisplay:"no data"}
+        </Text>
+        <Text
+          style={{
+            backgroundColor: '#2E2EFEaf',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -5]}],
+          }}>
+        Departure: {this.state.foliData.length > 0 ? this.state.foliData[0].originaimeddeparturetime:"no data"}
+        </Text>
+        <VrButton
+            style={{
+                backgroundColor: '#2E2EFEaf',
+            }}
+            onClick={()=> this.fetchData()}>
+            <Text
+              style={{
+              backgroundColor: '#2E2EFEaf',
+              fontSize: 0.8,
+              fontWeight: '400',
+              layoutOrigin: [0.5, 0.8],
+              paddingLeft: 0.2,
+              paddingRight: 0.2,
+              textAlign: 'center',
+              textAlignVertical: 'center',
+              transform: [{translate: [0, -0.5, -5]}],
+              }}>
+            update
+            </Text>
+        </VrButton>
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('vr_workshop', () => vr_workshop);
